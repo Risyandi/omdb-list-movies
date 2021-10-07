3c0b9e9f3e9aedcf3c28ddaa52759f390ebda19a
Risyandi <Risyandi.94@gmail.com>
2021-10-07 16:22:37 +0700

    fix: update App file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import logo from './assets/img/logo.svg';
+import React, {Suspense} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import routes from './routes';
+import PageNotFound from './common/component/pageNotFound';
 import './assets/css/App.css';
+
+const loading = () => (
+  <div className="loading"> 
+    <div>Loading...</div>
+  </div> 
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={loading()}>
+        <Switch>
+          {
+            routes.map((route, index) => (
+              route.component ? (
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  name={route.name}
+                  render={props => <route.component {...props} />}
+                />
+              ) : (null)
+            ))
+          }
+          <Route component={PageNotFound}/>
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
