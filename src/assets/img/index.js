3a707817409d2174d1087ce4cfe5126bd2367146
Risyandi <Risyandi.94@gmail.com>
2021-10-07 16:21:03 +0700

    fix: update assets & index

---
+++ src/assets/img/index.js
@@ -0,0 +1,16 @@
+/**
+ * created by : Risyandi
+ * 2021
+ */
+
+ import logo from '../img/logo.svg';
+ import posteBlank from '../img/poster-blank.png';
+ 
+ const img = {
+     logo,
+     posteBlank,
+ }
+ 
+ export {
+     img
+ }
