e28359b92720143f660c0e6866f992b92321c899
Risyandi <Risyandi.94@gmail.com>
2021-10-07 16:21:33 +0700

    fix: update link enpoint api

--- src/constant/endpointApi.js
+++ src/constant/endpointApi.js
@@ -1,2 +1,7 @@
-let endpointApi = 'http://www.omdbapi.com/?apikey=2c070c&';
+/**
+ * created by : Risyandi
+ * 2021
+ */
+
+let endpointApi = 'http://www.omdbapi.com/?apikey=2c070c';
 export const ENDPOINT_URL = endpointApi;
