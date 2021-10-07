267d6abce37afcd67e4e7398eccaaf783c2d223c
Risyandi <Risyandi.94@gmail.com>
2021-10-07 16:20:30 +0700

    fix: update routes

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+/**
+ * created by : Risyandi
+ * 2021
+ */
+
+import React from 'react';
+
+const Homepage = React.lazy(() => import('./pages/Homepage'));
+const Moviespage = React.lazy(() => import('./pages/Movies'));
+
+const routes = [{
+        path: '/',
+        exact: true,
+        name: 'Homepage',
+        component: Homepage,
+        typeLayout: 'full-layout'
+    },
+    {
+        path: '/movies',
+        restricted: true,
+        exact: true,
+        name: 'Movies',
+        component: Moviespage,
+        typeLayout: 'full-layout'
+    },
+    {
+        path: '/movies/detail',
+        restricted: true,
+        exact: true,
+        name: 'Detail Movies',
+        component: Moviespage,
+        typeLayout: 'full-layout'
+    }
+];
+
+export default routes;
